050a5b3d659e426de15a6b9c70f19648bb0196df
Virginia Herrero <175484721+herrerovir@users.noreply.github.com>
2024-11-13 15:24:48 +0100

    Add bivariate analysis

--- SQL-US-household-income-exploratory-data-analysis.sql
+++ SQL-US-household-income-exploratory-data-analysis.sql
@@ -56,3 +56,89 @@ FROM us_income_statistics_staging;
 -- This query retrieves the median income
 SELECT ROUND(AVG(`Median`), 0) AS Median_Income
 FROM us_income_statistics_staging;
+
+/* BIVARIATE ANALYSIS: This analysis consist in finding relations between two variables */
+
+-- Join both tables to collect useful data
+SELECT *
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id;
+    
+-- INCOME PER STATE
+-- Income distribution per state
+-- This query selects the average mean income per state ordered by lowest income
+SELECT g.`State_Name`, ROUND(AVG(`Mean`), 1) AS Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY g.`State_Name`
+ORDER BY 2;
+
+-- Top 5 states with lowest income
+-- This query fetches the 5 states with the lowest average income
+SELECT g.`State_Name`, ROUND(AVG(`Mean`), 1) AS Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY g.`State_Name`
+ORDER BY Avg_Income
+LIMIT 5;
+
+-- Top 5 states with highest average income
+-- This query selects the average mean income per state ordered by highest income
+SELECT g.`State_Name`, ROUND(AVG(`Mean`), 1) AS Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY g.`State_Name`
+ORDER BY Avg_Income DESC
+LIMIT 5;
+
+-- INCOME PER LOCATION TYPE
+-- Highest average income per Type of location
+SELECT `Type`, COUNT(`Type`) AS Count_Type, ROUND(AVG(`Mean`), 1) Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY `Type`
+ORDER BY Avg_Income DESC;
+
+-- Filter data with higher count of location type
+SELECT `Type`, COUNT(`Type`) AS Count_Type, ROUND(AVG(`Mean`), 1) Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY `Type`
+HAVING COUNT(`Type`) > 100
+ORDER BY Avg_Income DESC;
+
+-- INCOME PER CITY
+-- Income distribution per city
+-- This query selects the average mean income per city ordered by highest income
+SELECT g.`State_Name`, `City`, ROUND(AVG(`Mean`), 1) AS Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY g.`State_Name`, `City`
+ORDER BY Avg_Income DESC;
+
+-- Top 5 cities with highest average income
+-- This query select the 5 cities with the highest average income
+SELECT g.`State_Name`, `City`, ROUND(AVG(`Mean`), 1) AS Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY g.`State_Name`, `City`
+ORDER BY Avg_Income DESC
+LIMIT 5;
+
+-- Top 5 city with lowest average income
+-- This query select the 5 cities with the lowest average income
+SELECT g.`State_Name`, `City`, ROUND(AVG(`Mean`), 1) AS Avg_Income
+FROM us_geographic_location_staging AS g
+INNER JOIN us_income_statistics_staging AS i 
+    ON g.Id = i.Id
+GROUP BY g.`State_Name`, `City`
+ORDER BY Avg_Income
+LIMIT 5;
